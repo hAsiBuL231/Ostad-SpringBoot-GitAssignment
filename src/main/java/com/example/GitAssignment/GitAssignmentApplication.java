a3565930e78cfa67e611fab0861a16c50ffc3917
Md Hasibul <hossainhasibul2@gmail.com>
2026-04-18 12:21:18 +0600

    init

---
+++ src/main/java/com/example/GitAssignment/GitAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.GitAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GitAssignment/GitAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GitAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
